b2dc3ff48c66f03d0b9cdcf06cae87eb0094a0eb
島松寿悟 <shima.shima0608@gmail.com>
2016-10-16 00:54:49 +0800

    モックアップ作成

--- views/plan/detail.php
+++ views/plan/detail.php
@@ -2,11 +2,431 @@
     $PlansController = new PlansController($db, $table_name, $action);
     $plans_record = $plansController->show($id);
 
-    $blog = mysqli_fetch_assoc($blog_record);
+    $plan = mysqli_fetch_assoc($plan_record);
 ?>
 
-<h2>記事詳細</h2>
-<div><?php echo $blog['title']; ?></div>
-<div><?php echo $blog['body']; ?></div>
+<body>
 
-<?php echo link_to('../index', '一覧へ'); ?>
+<!-- Page header -->
+
+  <div class="page-header">
+    
+        <p>プラン名<br>
+    <h1>京都紅葉デート</h1>
+
+
+
+    <p>＊訪問した月を選択してください。（複数選択可）<br>
+    10月</p>
+
+<p>＊訪問日を選択してください。<br>
+土日・祝祭日
+</p>
+
+</div>
+
+    <!-- /Page header -->
+
+
+<ul id="sortable1" class="droptrue">
+  <li class="ui-state-default">
+
+
+<dl class="syncer-acdn-faq">
+  <dt class="syncer-acdn" data-target="syncer-acdn-01">行き先①</dt>
+    
+    <dd id="syncer-acdn-01">
+            
+    
+            <!-- Body -->
+            <div class="panel-body">
+        <p>スポット名<br>
+    <h2>清水寺</h2></p>
+
+<p>京都</p>
+
+<p>＊混雑具合<br>
+ めちゃ混み
+</p>
+
+<p>＊滞在時間</p>
+<div style="display:inline-flex">
+<p>1</p>時間
+<p>30</p>分
+</div>
+
+<p>＊費用<br>
+<div style="display:inline-flex">
+<P>2000円</P>（目安）</p>
+</div>
+
+
+<p>＊メモ<br>
+<p>京都デートの定番スポット！
+カップルで存分に楽しめると思います。
+二人分の参拝料とお守りを購入してだいたい2000円くらいでした。
+歩きやすい靴でいくことをお勧めします(°▽°)</p>
+</p>
+
+<!-- <div style="display:inline-flex"> -->
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+<!-- </div> -->
+
+            <!-- /Body -->
+
+       
+        <!-- /Panel --></dd>　</dl>
+  </li>
+
+
+  <li class="ui-state-default">
+  <dl class="syncer-acdn-faq">
+  <dt class="syncer-acdn" data-target="syncer-acdn-101">移動経路①</dt>
+    
+    <dd id="syncer-acdn-101">
+    
+    
+            <!-- Body -->
+<div class="panel-body">
+                <p>＊交通手段の選択</p>
+                <div style="display:inline-flex">
+    <p>バス</p>
+</div>
+
+<p>＊所要時間</p>
+<div style="display:inline-flex">
+<p>0</p>時間
+<p>15</p>分
+</div>
+
+<p>＊費用<br>
+<p>230</p>円（目安）</p>
+
+<p>＊メモ<br>
+費用はバス１人分の運賃です。</p>
+
+</div>
+            <!-- /Body -->
+    
+
+        <!-- /Panel --></dd></dl>
+        </li>
+
+
+  <li class="ui-state-default">
+  <dl class="syncer-acdn-faq">
+  <dt class="syncer-acdn" data-target="syncer-acdn-02">行き先②</dt>
+    <dd id="syncer-acdn-02">
+
+            
+    
+            <!-- Body -->
+            <div class="panel-body">
+        <p>スポット名<br>
+    <h2>清水寺</h2></p>
+
+<p>京都</p>
+
+<p>＊混雑具合<br>
+ めちゃ混み
+</p>
+
+<p>＊滞在時間</p>
+<div style="display:inline-flex">
+<p>1</p>時間
+<p>30</p>分
+</div>
+
+<p>＊費用<br>
+<div style="display:inline-flex">
+<P>2000円</P>（目安）</p>
+</div>
+
+
+<p>＊メモ<br>
+<p>京都デートの定番スポット！
+カップルで存分に楽しめると思います。
+二人分の参拝料とお守りを購入してだいたい2000円くらいでした。
+歩きやすい靴でいくことをお勧めします(°▽°)</p>
+</p>
+
+<!-- <div style="display:inline-flex"> -->
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+<!-- </div> -->
+
+            <!-- /Body -->
+
+     
+        <!-- /Panel --></dd>　</dl>
+</li>
+
+
+  <li class="ui-state-default">
+  <dl class="syncer-acdn-faq">
+  <dt class="syncer-acdn" data-target="syncer-acdn-102">移動経路②</dt>
+    
+    <dd id="syncer-acdn-102">
+    
+    
+           <!-- Body -->
+<div class="panel-body">
+                <p>＊交通手段の選択</p>
+                <div style="display:inline-flex">
+    <p>バス</p>
+</div>
+
+<p>＊所要時間</p>
+<div style="display:inline-flex">
+<p>0</p>時間
+<p>15</p>分
+</div>
+
+<p>＊費用<br>
+<p>230</p>円（目安）</p>
+
+<p>＊メモ<br>
+費用はバス１人分の運賃です。</p>
+
+</div>
+            <!-- /Body -->
+    
+    
+
+        <!-- /Panel --></dd></dl>
+        </li>
+
+  <li class="ui-state-default">
+  <dl class="syncer-acdn-faq">
+  <dt class="syncer-acdn" data-target="syncer-acdn-03">行き先③</dt>
+    <dd id="syncer-acdn-03">
+
+            
+    
+           <!-- Body -->
+            <div class="panel-body">
+        <p>スポット名<br>
+    <h2>清水寺</h2></p>
+
+<p>京都</p>
+
+<p>＊混雑具合<br>
+ めちゃ混み
+</p>
+
+<p>＊滞在時間</p>
+<div style="display:inline-flex">
+<p>1</p>時間
+<p>30</p>分
+</div>
+
+<p>＊費用<br>
+<div style="display:inline-flex">
+<P>2000円</P>（目安）</p>
+</div>
+
+
+<p>＊メモ<br>
+<p>京都デートの定番スポット！
+カップルで存分に楽しめると思います。
+二人分の参拝料とお守りを購入してだいたい2000円くらいでした。
+歩きやすい靴でいくことをお勧めします(°▽°)</p>
+</p>
+
+<!-- <div style="display:inline-flex"> -->
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+ <div class="panel-body">
+                <img class="img-responsive img-rounded" src="//placehold.it/220x150" />
+            </div>
+<!-- </div> -->
+
+            <!-- /Body -->
+
+     
+        <!-- /Panel --></dd>　</dl>
+        </li>
+</ul>
+ 
+
+
+ 
+<br style="clear:both">
+
+<div class="like">
+  <a class="btn icon-btn btn-primary" href="#"><span class="glyphicon btn-glyphicon glyphicon-thumbs-up img-circle text-primary"></span>Like</a>
+</div>
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <h2 class="page-header">コメント</h2>
+        <section class="comment-list">
+          <!-- First Comment -->
+          <article class="row">
+            <div class="col-md-2 col-sm-2 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+            <div class="col-md-10 col-sm-10">
+              <div class="panel panel-default arrow left">
+                <div class="panel-body">
+                  <header class="text-left">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <!-- Second Comment Reply -->
+          <article class="row">
+            <div class="col-md-2 col-sm-2 col-md-offset-1 col-sm-offset-0 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+            <div class="col-md-9 col-sm-9">
+              <div class="panel panel-default arrow left">
+                <div class="panel-heading right">Reply</div>
+                <div class="panel-body">
+                  <header class="text-left">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <!-- Third Comment -->
+          <article class="row">
+            <div class="col-md-10 col-sm-10">
+              <div class="panel panel-default arrow right">
+                <div class="panel-body">
+                  <header class="text-right">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2 col-sm-2 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+          </article>
+          <!-- Fourth Comment -->
+          <article class="row">
+            <div class="col-md-2 col-sm-2 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+            <div class="col-md-10 col-sm-10 col-xs-12">
+              <div class="panel panel-default arrow left">
+                <div class="panel-body">
+                  <header class="text-left">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <!-- Fifth Comment -->
+          <article class="row">
+            <div class="col-md-10 col-sm-10">
+              <div class="panel panel-default arrow right">
+                <div class="panel-body">
+                  <header class="text-right">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2 col-sm-2 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+          </article>
+          <!-- Sixth Comment Reply -->
+          <article class="row">
+            <div class="col-md-9 col-sm-9 col-md-offset-1 col-md-pull-1 col-sm-offset-0">
+              <div class="panel panel-default arrow right">
+                <div class="panel-heading">Reply</div>
+                <div class="panel-body">
+                  <header class="text-right">
+                    <div class="comment-user"><i class="fa fa-user"></i> That Guy</div>
+                    <time class="comment-date" datetime="16-12-2014 01:05"><i class="fa fa-clock-o"></i> Dec 16, 2014</time>
+                  </header>
+                  <div class="comment-post">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                  <p class="text-right"><a href="#" class="btn btn-default btn-sm"><i class="fa fa-reply"></i> reply</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2 col-sm-2 col-md-pull-1 hidden-xs">
+              <figure class="thumbnail">
+                <img class="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
+                <figcaption class="text-center">username</figcaption>
+              </figure>
+            </div>
+          </article>
+        </section>
+    </div>
+  </div>
+</div>
